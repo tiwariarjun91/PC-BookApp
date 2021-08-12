6702f653c1c15382c75240d1c68943bfdee67db3
tiwariarjun91 <tiwariarjun91@gmail.com>
2021-08-13 01:49:38 +0530

    Created sample Laptop

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/tiwariarjun91/PC-BookApp/sample"
 )
 
 func main(){
 	fmt.Println("Hello World !!!")
+	fmt.Println(sample.NewLaptop())
 }
--- sample/random.go
+++ sample/random.go
@@ -3,6 +3,8 @@ package sample
 import(
 	"math/rand"
 	"github.com/tiwariarjun91/PC-BookApp/pb"
+	"github.com/google/uuid"
+	//"time"
 
 )
 func randomKeyBoardLayout() pb.Keyboard_Layout{
@@ -40,18 +42,100 @@ func randomCPUName(brand string) string{
 		"Core i9-9980HK",
 		"Core i7-9750H",
 		"Core i5-9400F",
-		"Core i3-1005G1"
+		"Core i3-1005G1", //needs comma on last line aswell
 	)
 	}
 
 	return randomStringFromSet(
 		"Ryzen 7 Pro 2700U",
 		"Ryzen 5 Pro 3500U",
-		"Ryzen 3 Pro 3200GE"
+		"Ryzen 3 Pro 3200GE", //needs comma on last line as well
 	)
 	
 }
 
 func randomInt(min,max int) int{
 	return min + rand.Intn(max-min+1)
+}
+
+func randomFloat64(min,max float64) float64{
+	return min + rand.Float64()*(max-min)
+}
+func randomFloat32(min, max float32) float32 {
+	return min + rand.Float32()*(max-min)
+}
+func randomGPUBrand() string{
+	return randomStringFromSet("Nvidia","AMD")
+}
+
+func randomId() string {
+	return uuid.New().String()
+}
+
+func randomGPUName(brand string) string{
+	if brand == "Nvidia"{
+		return randomStringFromSet(
+			"RTX 2060",
+			"RTX 2070",
+			"GTX 1660-Ti",
+			"GTX 1070",
+		)
+	}
+
+	return randomStringFromSet(
+		"RX 590",
+		"RX 580",
+		"RX 5700-XT",
+		"RX Vega-56",
+	)
+}
+/* 
+func randomStringFromSet(a ...string) string{
+	n := len(a)
+	if a == 0{
+		return ""
+	}
+
+	return a[rand.Intn(n)]
+}
+*/
+
+/*
+func RandomInt(min,max int) int{
+	return min + rand.Intn(max-min)
+}*/
+
+func randomScreenResolution() *pb.Screen_Resolution {
+	height := randomInt(1080,4320)
+	width := height * 16 /9
+
+	screenResolution := &pb.Screen_Resolution{
+		Height : uint32(height),
+		Width : uint32(width),
+	}
+
+	return screenResolution
+}
+
+func randomScreenPanel() pb.Screen_Panel{
+	if rand.Intn(2) == 1{
+		return pb.Screen_IPS
+	}else{
+		return pb.Screen_OLED
+	}
+}
+
+func randomLaptopBrand() string{
+	return randomStringFromSet("Apple","Dell", "Lenovo")
+}
+
+func randomLaptopName(brand string) string{
+	switch brand{
+	case "Apple":
+		return randomStringFromSet("Macbook Air", "Macbook Pro")
+	case "Dell":
+		return randomStringFromSet("Latitude", "Vostro", "XPS", "Alienware")
+	default:
+		return randomStringFromSet("Thinkpad X1", "Thinkpad P1", "Thinkpad P53")
+	}
 }
