03d200a40e3c4dd7e08c54e576275df269e036af
tiwariarjun91 <tiwariarjun91@gmail.com>
2021-08-20 01:34:44 +0530

    Timeout Context

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -9,6 +9,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
 	"google.golang.org/grpc/codes"
+	"time"
 
 
 )
@@ -32,7 +33,10 @@ func main(){
 		Laptop : laptop,
 	}
 
-	res,err := laptopClient.CreateLaptop(context.Background(),req)
+	ctx,cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res,err := laptopClient.CreateLaptop(ctx,req)
 	if err != nil{
 		st,ok := status.FromError(err)
 		if ok && st.Code() == codes.AlreadyExists {
